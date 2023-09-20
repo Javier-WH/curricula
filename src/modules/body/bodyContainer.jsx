fc8a0b0f44725d942b2e95c0ec19c0052313d3dd
unknown <fj_rh@hotmail.com>
2023-09-20 17:43:42 -0400

    svg added

--- src/modules/body/bodyContainer.jsx
+++ src/modules/body/bodyContainer.jsx
@@ -7,15 +7,51 @@ export default function BodyContainer() {
             <div className="body-module">
                 <div className="body-module-title">CAREER OBJECTIVE</div>
                 <p className="body-module-text">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor optio voluptate, vero assumenda dolore molestiae hic autem error ratione quas incidunt, accusantium aut exercitationem consequuntur odio perferendis aperiam non. Assumenda.
+                    Como ingeniero en sistemas egresado de la UPTLL, mi objetivo es aplicar mis habilidades y conocimientos en el campo de la tecnología de la información, enfocándome en contribuir al desarrollo y promoción del software libre. Con una sólida experiencia tanto en empresas locales como en proyectos como freelancer, deseo ahora embarcarme en desafiantes proyectos importantes, donde pueda emplear nuevas tecnologías y expandir mis habilidades como programador.
+                </p>
+                <p className="body-module-text">
+                    Mis principales atributos incluyen una sólida formación en ingeniería de sistemas, que me ha proporcionado un sólido conocimiento en el diseño, desarrollo y mantenimiento de software. Además, mi enfoque en el software libre me ha permitido adquirir una comprensión profunda de la filosofía y las mejores prácticas asociadas a este movimiento.
+                </p>
+                <p className="body-module-text">
+                    A lo largo de mi carrera, he trabajado en diversos entornos, lo que me ha brindado la capacidad de adaptarme rápidamente a diferentes contextos y equipos de trabajo. Mi experiencia como freelancer me ha permitido desarrollar habilidades de autodisciplina, gestión del tiempo y resolución de problemas de manera independiente.
+                </p>
+                <p className="body-module-text">
+                    Mi pasión por la tecnología y el deseo de estar a la vanguardia de las nuevas tendencias me motiva a buscar proyectos desafiantes y de alto impacto. Busco una oportunidad donde pueda colaborar con un equipo talentoso y comprometido en proyectos que utilicen nuevas tecnologías, lo que me permitirá seguir aprendiendo y creciendo profesionalmente.
                 </p>
             </div>
 
             <div className="body-module">
                 <div className="body-module-title">WORK EXPERIENCE</div>
-                <p className="body-module-text">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor optio voluptate, vero assumenda dolore molestiae hic autem error ratione quas incidunt, accusantium aut exercitationem consequuntur odio perferendis aperiam non. Assumenda.
-                </p>
+                <ul>
+                    <li>
+                        <span className="body-module-subtitle">U.E.C. Batalla de la Victoria</span>
+                        <span>
+                            Desarrollo de un sistema de gestión de calificaciones e inscripción de estudiantes, que incluía los siguientes componentes:
+                        </span>
+                        <ol>
+                            <li>Frontend: una página web creada con React, desde la cual los profesores podían subir las calificaciones de los estudiantes.</li>
+                            <li>Backend: una API en Node.js y Express para manejar las solicitudes del frontend, junto con una base de datos en PostgreSQL.</li>
+                            <li>Aplicación para Android desarrollada con Kotlin.</li>
+                        </ol>
+                    </li>
+                    <li>
+                        <span className="body-module-subtitle">U.E. Abelardo Méndez</span>
+                        <span>
+                            Desarrollo de un sistema de registro de asistencias, implementado en Java y haciendo uso de un lector de huellas dactilares.
+                        </span>
+                    </li>
+                    <li>
+                        <span className="body-module-subtitle">Hidrocapital</span>
+                        <span>
+                            Desarrollo de un sistema de monitoreo de equipos hidráulicos, que involucraba la obtención de información proveniente de múltiples sensores en diversos equipos. El sistema emitía alarmas en caso de situaciones que lo requirieran y también generaba alertas para indicar la falta de mantenimiento.
+                        </span>
+                    </li>
+                    <br />
+                    <li >
+                        Desarrollo de múltiples páginas web para diversos clientes locales.
+                    </li>
+                </ul>
+
             </div>
 
             <div className="body-module">
@@ -28,16 +64,27 @@ export default function BodyContainer() {
         <div>
             <div className="body-module">
                 <div className="body-module-title">EDUCATION</div>
-                <p className="body-module-text">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor optio voluptate, vero assumenda dolore molestiae hic autem error ratione quas incidunt, accusantium aut exercitationem consequuntur odio perferendis aperiam non. Assumenda.
-                </p>
+                <ul className="body-module-text">
+                   <li>Ingeniero en Sistemas, especialidad en desarrollo de software</li>
+                   <li>Instituto Tecnologico de los Llanos</li>
+                   <li>Septiembre 2016 - Junio 2020</li>
+                   <li>Altagracia de Orituco, Edo Guarico, Venezuela</li>
+                   <li>GPA: 3,81</li>
+                </ul>
             </div>
 
             <div className="body-module">
                 <div className="body-module-title">RELEVANT COURSES</div>
-                <p className="body-module-text">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor optio voluptate, vero assumenda dolore molestiae hic autem error ratione quas incidunt, accusantium aut exercitationem consequuntur odio perferendis aperiam non. Assumenda.
-                </p>
+                <ul className="body-module-text">
+                  <li>Curso de HTML y CSS, Platzi</li>
+                  <li>Curso de Fundamentos de Ingeniería de Software, Platzi</li>
+                  <li>Curso de diseño responsivo, Platzi</li>
+                  <li>Curso de estructuras de datos y algoritmos, Udemy</li>
+                  <li>Estructura de código de C#, Microsoft</li>
+                 <li>Curso de Introducción al Desarrollo Web: HTML y CSS, Google</li>
+                 <li>Curso intensivo de aprendizaje automático, Google</li>
+                 <li>Certificado Profesional de Análisis de datos, Google</li>
+                </ul>
             </div>
 
             <div className="body-module">
